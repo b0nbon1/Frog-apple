b22325a1f05aaf7ab542b9c4ed0f8a7116a05954
Bonvic Bundi <nyabuyabonvic@gmail.com>
2020-04-30 06:08:29 +0200

    credits and issues

--- src/app.js
+++ src/app.js
@@ -13,6 +13,13 @@ const app = () => {
     >
       <h1>Use Arrow Keys to move the frog</h1>
       <World />
+      <h6>
+        {' '}
+        it&rsquo;s an open source game, raise an issue to make it better:
+        <a href='https://github.com/b0nbon1/Frog-apple/issues'>
+          b0nbon1/frog-apples
+        </a>
+      </h6>
     </div>
   );
 };
